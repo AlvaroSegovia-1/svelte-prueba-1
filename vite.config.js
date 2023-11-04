fd8420ba0a95c919f1555f7aa20912b2b446c719
Alvagit25 <alvarodesegovia@gmail.com>
2023-11-04 10:19:55 +0100

    cambio dist

--- vite.config.js
+++ vite.config.js
@@ -4,16 +4,16 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 
 
-/* export default defineConfig({
+export default defineConfig({
   plugins: [svelte()],
-}) */
+})
 
 
 
-export default defineConfig(({ command }) => {
+/* export default defineConfig(({ command }) => {
   const isProduction = command === 'build';
   return {
     plugins: [svelte()],
     base: isProduction ? '/' : '/',
   };
-});
+}); */
