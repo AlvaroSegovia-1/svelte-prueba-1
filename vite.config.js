60b432eaaeae6b74b06e9b30b90a44126478bf4e
Alvagit25 <alvarodesegovia@gmail.com>
2023-11-04 10:12:14 +0100

    cambio vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,18 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+
+
+/* export default defineConfig({
   plugins: [svelte()],
-})
+}) */
+
+
+
+export default defineConfig(({ command }) => {
+  const isProduction = command === 'build';
+  return {
+    plugins: [svelte()],
+    base: isProduction ? '/' : '/',
+  };
+});
